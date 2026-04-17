5322e723a68b9b1e3a16241139c771c877471068
Ritesh-ScriptHub <vritesh90833@gmail.com>
2026-04-17 13:22:13 +0530

    corrected the pakage  name

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -26,6 +26,6 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-const userModel = moongoose.model("user", userSchema);
+const userModel = mongoose.model("user", userSchema);
 
 module.exports = userModel;
