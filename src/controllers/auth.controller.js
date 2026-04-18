b409319621db0207ab4296b1d6196b6d96f86e76
Ritesh-ScriptHub <vritesh90833@gmail.com>
2026-04-18 10:48:53 +0530

    add login functionality to auth controller and routes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -54,4 +54,48 @@ async function registerUser(req, res) {
     }
 }
 
-module.exports = {registerUser}
+async function loginUser(req, res) {
+
+    try {
+        const{username, email, password} = req.body;
+
+        const user = await userModel.findOne({
+            $or: [
+                {username},
+                {email}
+            ]
+        })
+
+        if(!user) {
+            return res.status(401).json({ message: "Credential failed" })
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid) {
+            return res.status(401).json({message:"Credential failed"})
+        }
+
+        const token = await jwt.sign({
+            id: user._id,
+            role: user.role
+        }, process.env.JWT_SECRET)
+
+        res.cookie("token", token);
+
+        res.status(200).json({
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                role: user.role
+            }
+        })
+    } catch (error) {
+        // console.error("LogIn error:", error);
+        res.status(500).json({message: "Internal server error", error: error.message});
+    }
+
+}
+
+module.exports = {registerUser, loginUser}
